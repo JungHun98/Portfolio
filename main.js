1b9f04a72de191129ac1511e0c445009b06ea730
JungHun98 <j7papa@naver.com>
2022-02-09 14:03:10 +0900

    JS scroll&click event

--- main.js
+++ main.js
@@ -2,6 +2,12 @@
 
 let header = document.querySelector('#header');
 let headerHeight = header.clientHeight;
+let home = document.querySelector('#home');
+let homeNote = document.querySelector('.home__box');
+let homeHeight = home.getBoundingClientRect().height;
+
+let menuArray = document.querySelectorAll('.header__menu__item');
+let arrowUp = document.querySelector('.arrowup');
 
 //스크롤 시 header의 backgound의 색이 진해짐 
 document.addEventListener('scroll', () => {
@@ -11,9 +17,15 @@ document.addEventListener('scroll', () => {
   else{
     header.classList.remove('header--dark');
   }
-  
-  let menuArray = document.querySelectorAll('.header__menu__item');
+  homeNote.style.opacity = 1 - window.scrollY / homeHeight;
 
+  if(window.scrollY > headerHeight){
+    arrowUp.classList.add('visible');
+  }
+  else{
+    arrowUp.classList.remove('visible');
+
+  }
 });
 
 // header, contact click시 스크롤 이동
@@ -23,11 +35,15 @@ document.addEventListener('click', (event) => {
     return;
   }
   else{
-    let movePoint = document.querySelector(moveId)
-    movePoint.scrollIntoView({behavior: "smooth"});
+    scrollIntoView(moveId);
   }
 })
 
-console.log(document.querySelectorAll('.header__menu__item'))
-// about 500
-// skills 1137
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+})
+
+function scrollIntoView(selector){
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+}
